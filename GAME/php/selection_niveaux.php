68aed97cdd495fa6917f9b4bf8a37f77a36f08bd
Achot <achot.barseghyan@gmail.com>
2020-06-08 21:00:40 +0200

    début page : selection niveaux

---
+++ GAME/php/selection_niveaux.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="../css/selection_niveaux.css">
+    <title>Pendu - Connexion</title>
+</head>
+
+<body>
+    <main>
+        <div class="profil-picture">
+            <a href="#">
+                <div class="profil-container">
+                    <img src="../img/account_circle-white-18dp.svg" alt="profil Picture">
+                </div>
+            </a>
+        </div>
+        <h1>Selection de niveaux</h1>
+
+        <div class="levels">
+            <div class="level1"><span>1</span></div>
+            <div class="level2"><span>2</span></div>
+            <div class="level3"><span>3</span></div>
+            <div class="level4"><span>4</span></div>
+            <div class="level5"><span>5</span></div>
+            <div class="level6"><span>6</span></div>
+            <div class="level7"><span>7</span></div>
+            <div class="level8"><span>8</span></div>
+            <div class="level9"><span>9</span></div>
+            <div class="level10"><span>10</span></div>
+        </div>
+    </main>
+</body>
+
+</html>
