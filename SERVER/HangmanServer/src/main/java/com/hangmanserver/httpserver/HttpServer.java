bedbd56debc5595ae1a7eda53cf65958dbc2a6fb
iloxe <fifoualex73@gmail.com>
2020-06-11 18:48:02 +0200

    conitnué

--- SERVER/HangmanServer/src/main/java/com/hangmanserver/httpserver/HttpServer.java
+++ SERVER/HangmanServer/src/main/java/com/hangmanserver/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package com.hangmanserver.httpserver;
 import com.hangmanserver.httpserver.config.Configuration;
 import com.hangmanserver.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * Driver class for the Http Server
@@ -20,5 +26,34 @@ public class HttpServer {
 
         System.out.println("Using Port:" + conf.getPort());
         System.out.println("Using Webroot:" + conf.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Java HTTP Server</title></head><body><h1>simple java HTTP Server</h1></body></html>";
+
+            final String CRLF = "\n\r"; //(CRLF = character line feed)
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF +  //Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                    "Content-Length: " + html.getBytes().length + CRLF + //HEADER
+                            CRLF +
+                            html +
+                            CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ SERVER/HangmanServer/src/main/java/com/hangmanserver/httpserver/core/ServerListenerThread.java
@@ -0,0 +1,9 @@
+package com.hangmanserver.httpserver.core;
+
+
+public class ServerListenerThread extends Thread {
+
+    @Override
+    public void run() {
+    }
+}
