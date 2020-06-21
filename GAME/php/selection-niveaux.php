a1dc221f9609bbf260254c75feee7be6b5d30aa8
Achot <achot.barseghyan@gmail.com>
2020-06-21 20:12:33 +0200

    update selection niveaux

--- GAME/php/selection-niveaux.php
+++ GAME/php/selection-niveaux.php
@@ -27,22 +27,69 @@ if (!isset($_SESSION['UserName'])) {
             <a href="#">
                 <div class="profil-container">
                     <img src="../img/account_circle-white-18dp.svg" alt="profil Picture">
+                    <span class="username-session">
+                        <?php 
+                            if (isset($_SESSION['UserName'])) {
+                                echo $_SESSION['UserName'];
+                                echo ' <a class="logout-btn waves-effect waves-light btn" href="logout.php?logout">logout</a> ';
+                            }else{
+
+                            }
+                        ?>
+                    </span>
                 </div>
             </a>
         </div>
         <h1>Selection de niveaux</h1>
 
         <div class="levels">
-            <div class="level1"><span>Level 1</span></div>
-            <div class="level2"><span>Level 2</span></div>
-            <div class="level3-1"><span>Level 3-1</span></div>
-            <div class="level4-1"><span>Level 4-1</span></div>
-            <div class="level3-2"><span>Level 3-2</span></div>
-            <div class="level4-2"><span>Level 4-2</span></div>
-            <div class="level5"><span>Level 5</span></div>
-            <div class="level6"><span>Level 6</span></div>
-            <div class="level7"><span>Level 7</span></div>
-            <div class="level-bonus"><span>Level 8</span></div>
+            <div class="level1"><span class="center-align">Level 1</span></div>
+
+            <div class="level2 locked">
+                <span class="center-align">Level 2</span>
+                <i class="material-icons lock">lock</i>
+            </div>
+
+            <div class="level3-1 locked">
+                <span class="center-align">Level 3-1</span>
+                <i class="material-icons lock">lock</i>
+            </div>
+
+            <div class="level4-1 locked">
+                <span class="center-align">Level 4-1</span>
+                <i class="material-icons lock">lock</i>
+            </div>
+
+            <div class="level3-2 locked">
+                <span class="center-align">Level 3-2</span>
+                <i class="material-icons lock">lock</i>
+            </div>
+
+            <div class="level4-2 locked">
+                <span class="center-align">Level 4-2</span>
+                <i class="material-icons lock">lock</i>
+            </div>
+
+            <div class="level5 locked">
+                <span class="center-align">Level 5</span>
+                <i class="material-icons lock">lock</i>
+            </div>
+
+            <div class="level6 locked">
+                <span class="center-align">Level 6</span>
+                <i class="material-icons lock">lock</i>
+            </div>
+
+            <div class="level7 locked">
+                <span class="center-align">Level 7</span>
+                <i class="material-icons lock">lock</i>
+            </div>
+
+            <div class="level-bonus locked">
+                <span class="center-align">Magasin</span>
+                <i class="material-icons lock">lock</i>
+            </div>
+
         </div>
     </main>
 </body>
