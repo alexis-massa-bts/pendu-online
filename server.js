948dc5565a7ed0ed533ef592116be274de30ede3
iloxe <fifoualex73@gmail.com>
2020-11-08 09:55:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-// var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-
-//  app.get('/', function(req, res){
-//      res.sendFile(__dirname + '/client/index.php');
-//  })
-
-// io.on('connection', function(socket){
-//     console.log("User connected");
-//     socket.on('disconnect', function(){
-//         console.log("User disconnected");
-//     })
-
-// })
-
-// http.listen(8080, function(){
-//     console.log("Server running on 8080");
-// });
-
 var express = require('express');
 var app = express();
 
